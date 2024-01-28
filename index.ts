cdfe559c546087bb023606ea827bef6c2d0f6cb2
Nithin Saji <nithin111@gmail.com>
2024-01-28 14:36:57 +0530

    tune endpointing for better experience

--- index.ts
+++ index.ts
@@ -8,7 +8,8 @@ import dotenv from 'dotenv';
 import { getNewChatbot } from './assistant';
 import { createSpeech } from './tts'
 dotenv.config();
-
+  
+// TODO: when we recive an intermediate result, stop playing for 2 seconds time we recieve a speach final.
 const openai = new OpenAi();
 
 const app: Express = express();
@@ -29,6 +30,8 @@ function bindDeepgramEvents(connection: LiveClient, socket: Socket, chatbot: any
   connection.on(LiveTranscriptionEvents.Transcript, async (data: any) => {
     // check if the transcript is final and has any words
     // console.log(data)
+    console.log("transcript recieved");
+    console.log(data.channel.alternatives[0].transcript)
     if (data.speech_final && data.channel.alternatives[0].transcript.length > 0) {
       socket.emit('transcript', data.channel.alternatives[0].transcript);
       console.log("user:", data.channel.alternatives[0].transcript);
@@ -52,6 +55,11 @@ function bindDeepgramEvents(connection: LiveClient, socket: Socket, chatbot: any
       });
 
 
+    } else {
+      if (data.channel.alternatives[0].transcript.length > 0) {
+        console.log("no words");
+      }
+      console.log(data);  
     }
 
   });
@@ -73,13 +81,14 @@ io.on('connection', (socket: Socket) => {
    You will answer customer queries and book an appoinment for detailed consultation after asking a few questions.
     you will recieve the questions from the customer and you will answer them with only the response for the question.
      You will speak in short sentences and you will make sure that the customer understands you.`);
-    //  TODO: in case there is significant delay in input, we need confirm with the user whether they are following
+  //  TODO: in case there is significant delay in input, we need confirm with the user whether they are following
   if (!connection || connection.getReadyState() !== LiveConnectionState.OPEN) {
     connection = deepgram.listen.live({
       smart_format: true,
       model: 'nova-2',
       language: 'en-US',
       speech_final: true,
+      endpointing: 500,
     });
     console.log("connecting to deepgram");
 
