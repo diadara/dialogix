5f3921af5a9f801d004599a02becabb0af8ceeba
Nithin Saji <nithin111@gmail.com>
2024-01-29 23:04:37 +0530

    fix fly io deployment

--- load_env_to_fly.sh
+++ load_env_to_fly.sh
@@ -11,6 +11,9 @@ do
     # Split the line into name and value
     IFS='=' read -ra parts <<< "$line"
 
+    # Remove quotes from the value
+    parts[1]=$(echo "${parts[1]}" | tr -d '"')
+
     # Use fly secrets set command to set the variable
     fly secrets set "${parts[0]}=${parts[1]}"
 
